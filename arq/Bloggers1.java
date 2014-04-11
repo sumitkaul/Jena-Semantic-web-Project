d4af20410fee5ebd72fbbd239d089b1359a27804
Sumit Kaul <sumit.kaul@creativesyscon.com>
2014-04-11 12:11:47 -0400

    initial commit

---
+++ arq/Bloggers1.java
@@ -0,0 +1,50 @@
+
+import com.hp.hpl.jena.rdf.model.*;
+import com.hp.hpl.jena.util.FileManager;
+import com.hp.hpl.jena.query.* ;
+import com.hp.hpl.jena.query.ARQ;
+import com.hp.hpl.jena.sparql.*;
+
+import java.io.*;
+
+public class Bloggers1 extends Object {
+	
+		static final String inputFileName = "bloggers.rdf";
+        
+      	public static void main (String args[]) {
+
+		// Create an empty in-memory model 
+		Model model = ModelFactory.createDefaultModel();
+		
+		// use the FileManager to open the bloggers RDF graph from the filesystem
+        InputStream in = FileManager.get().open(inputFileName);
+        if (in == null) {
+            throw new IllegalArgumentException( "File: " + inputFileName + " not found");
+        }
+        
+        // read the RDF/XML file
+        model.read( in, "" );
+        
+		// Create a new query
+		String queryString = 
+			"PREFIX foaf: <http://xmlns.com/foaf/0.1/> " +
+			"SELECT ?name ?url " +
+			"WHERE {" +
+			"      ?x foaf:name ?name . " +
+			"      ?x foaf:weblog ?url . " +
+			"      }";
+
+		Query query = QueryFactory.create(queryString);
+
+		// Execute the query and obtain results
+		QueryExecution qe = QueryExecutionFactory.create(query, model);
+		ResultSet results = qe.execSelect();
+
+		// Output query results	
+		ResultSetFormatter.out(System.out, results, query);
+
+		// Important - free up resources used running the query
+		qe.close();
+	}
+	
+}
